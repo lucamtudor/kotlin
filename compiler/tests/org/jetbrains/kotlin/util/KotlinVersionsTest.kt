da4db44c9e5088a5dae0ca9a6b77986d1179d561
Ilya Gorbunov <ilya.gorbunov@jetbrains.com>
2017-05-18 08:19:47 +0300

    ~revert test

--- compiler/tests/org/jetbrains/kotlin/util/KotlinVersionsTest.kt
+++ compiler/tests/org/jetbrains/kotlin/util/KotlinVersionsTest.kt
@@ -18,7 +18,6 @@ package org.jetbrains.kotlin.util
 
 import com.intellij.openapi.util.io.FileUtil
 import com.intellij.util.Processor
-import com.intellij.util.text.VersionComparatorUtil
 import org.jetbrains.kotlin.codegen.forTestCompile.ForTestCompileRuntime
 import org.jetbrains.kotlin.config.KotlinCompilerVersion
 import org.jetbrains.kotlin.config.LanguageVersion
@@ -32,11 +31,6 @@ import java.util.*
 import javax.xml.parsers.SAXParserFactory
 
 class KotlinVersionsTest : KtUsefulTestCase() {
-
-    fun testVersionComparison() {
-        Assert.assertTrue(VersionComparatorUtil.compare("1.2-M2-release-IJ2016.1-2", "1.2-M10-release-IJ2016.1-2") < 0)
-    }
-
     fun testVersionsAreConsistent() {
         val versionPattern = "(\\d+)\\.(\\d+)(\\.(\\d+)|-SNAPSHOT)?".toRegex()
 
